3fa1855afb2d00ef0a051c38c9f60445defbaa27
joshuafortunatus <joshuafortunatus@gmail.com>
2025-11-22 21:20:34 -0700

    commit

--- scripts/fetch_nps_data.py
+++ scripts/fetch_nps_data.py
@@ -35,7 +35,8 @@ ENDPOINTS = {
     'tours': {'path': '/tours', 'table': 'nps_tours'},
     'thingstodo': {'path': '/thingstodo', 'table': 'nps_things_to_do'},
     'events': {'path': '/events', 'table': 'nps_events'},
-    'places': {'path': '/places', 'table': 'nps_places'}
+    'places': {'path': '/places', 'table': 'nps_places'},
+    'alerts': {'path': '/alerts', 'table': 'nps_src_places'}
 }
 
 def fetch_endpoint_data(endpoint_name, endpoint_path):
